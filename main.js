cdf3de289b08cd677980e74c0ba9540bad41914b
Meshail753 <73825714+Meshail753@users.noreply.github.com>
2022-05-29 16:43:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+status="";
+input="";
+function setup(){
+    canvas = createCanvas(250,250);
+    canvas.position(640,500);
+    video=createCapture(VIDEO);
+    video.hide();
+}
+function draw(){
+    image(video,0,0,250,250)
+}
+function start(){
+    objectdetector=ml5.objectDetector("cocossd",modeloaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    input = document.getElementById("input").value;
+}
+
+function modeloaded(){
+    console.log("cocossd linked.")
+    status=true;
+}
